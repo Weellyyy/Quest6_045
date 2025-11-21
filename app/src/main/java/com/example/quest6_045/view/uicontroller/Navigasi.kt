b04b31045931a017f1695eb0c7d04534e5938267
Weellyyy <wildanprtm2304@gmail.com>
2025-11-21 15:50:41 +0700

    mengambil instance SiswaViewModel yang dikelola oleh Compose untuk dipakai di UI

--- app/src/main/java/com/example/quest6_045/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/quest6_045/view/uicontroller/Navigasi.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.quest6_045.view.FormIsian
 import com.example.quest6_045.view.TampilData
+import com.example.quest6_045.viewmodel.SiswaViewModel
 
 enum class Navigasi{
     Formulirku,
@@ -17,7 +19,8 @@ enum class Navigasi{
 }
 
 @Composable
-fun DataApp(
+fun SiswaApp(
+    viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang->

f63c265a04e1916517ee54cbf6708d4c82c3622c
Weellyyy <wildanprtm2304@gmail.com>
2025-11-21 15:20:34 +0700

    membuat variable baru bernama statusUI

--- app/src/main/java/com/example/quest6_045/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/quest6_045/viewmodel/SiswaViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.quest6_045.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.quest6_045.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
 class SiswaViewModel : ViewModel(){
     private val _statusUI = MutableStateFlow(Siswa())
+
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 }

3ab2816276d52734bc43887d71052046f0cd64f4
Weellyyy <wildanprtm2304@gmail.com>
2025-11-21 15:23:58 +0700

    membuat fungsi baru dengan parameter ls

--- app/src/main/java/com/example/quest6_045/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/quest6_045/viewmodel/SiswaViewModel.kt
@@ -5,9 +5,16 @@ import com.example.quest6_045.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class SiswaViewModel : ViewModel(){
     private val _statusUI = MutableStateFlow(Siswa())
 
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls: MutableList<String>){
+        _statusUI.update{statusSaatIni->
+            statusSaatIni.copy(nama=ls[0], gender=ls[1], alamat=ls[2])
+        }
+    }
 }

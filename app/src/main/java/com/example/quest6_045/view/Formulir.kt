2f73e89426381181040be2075351109053f62a77
Weellyyy <wildanprtm2304@gmail.com>
2025-11-21 15:38:26 +0700

    merubah dan menyesuaikan untuk gender

--- app/src/main/java/com/example/quest6_045/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_045/view/Formulir.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -71,15 +72,23 @@ fun FormIsian(
                 .padding(20.dp)
                 .width(250.dp), thickness = Thickness, color = Color.Red)
             Row {
-                pilihanJK.forEach {
-                        item->
-                    Row (verticalAlignment = Alignment.CenterVertically){
+                pilihanJK.forEach { item->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender== item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
                         RadioButton(
-                            selected = false,
-                            onClick = {item}
+                            selected = txtGender== item,
+                            onClick = null
                         )
-                        Text(text = item)
-                    }
+                        Text(
+                            text = item,
+                            modifier = Modifier.padding(start= 4.dp)
+                        )
                 }
             }
             HorizontalDivider(modifier = Modifier
